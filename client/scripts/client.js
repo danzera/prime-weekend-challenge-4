05772a228bec2a8307695251392c63977fa67e1e
Dan Zera <dczera@gmail.com>
2017-04-02 12:12:53 -0500

    add property.js file - base file copied from another project

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,25 +1,14 @@
 $(document).ready(function() {
-  // start with a basic console.log
-  // TEST to ensure jQuery and our JS files are sourced into our HTML doc correctly
-  console.log('Meow, meow, meow...cat speak for JS/jQ files sourced');
-  // call init() function to establish base page setup
-  init();
+  initialize(); // establish initial user experience
 });
 
 // initial page setup
-function init() {
+function initialize() {
   // add event listeners to the page
   addEventListeners();
 }
 
 // any event listeners needed
 function addEventListeners() {
-  // a basic button click-listener
-  // uses the sayHi() function when #helloBtn is clicked
-  $('.jumbotron').on('click', '#helloBtn', sayHi);
-}
-
-// say 'hi' to the user
-function sayHi() {
-  alert('Well, hello there!');
+  // HARD MODE -- need some type of listeners for property addition
 }
